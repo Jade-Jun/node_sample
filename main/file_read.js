b891388c98a7462a20dec7cbbebc06c5cc025c27
전용수 <jys1385@gmail.com>
2017-09-25 09:25:06 +0900

    - 동기식 코드 추가

--- main/file_read.js
+++ main/file_read.js
@@ -1,6 +1,14 @@
 var fs = require("fs");
 
 /**
+ * 동기식 코드
+ */
+// var data = fs.readdirSync('main/input.txt');
+// console.log(data.toString());
+// console.log("program has end")
+
+/**
+ * 비동기식 코드
  * readfile 함수는 비동기식으로 파일을 읽는 함수
  * 첫번째 값으로는 error 두번째 값으로는 data를 받음
  */
@@ -9,4 +17,4 @@ fs.readFile('main/input.txt', function(err, data) {
     console.log(data.toString());
 });
 
-console.log('prograge has end');
+console.log('program has end');
