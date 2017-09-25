746bd19b490f828504105eb7c5d903207bc33f8a
전용수 <jys1385@gmail.com>
2017-09-25 18:09:37 +0900

    - ejs file을 이용한 loop 예제 (view에 파라미터 넘기기)

--- myExpressApp/routes/main.js
+++ myExpressApp/routes/main.js
@@ -1,9 +1,16 @@
-module.exports = function(app)
+module.exports = function(app, fs)
 {
-     app.get('/',function(req,res){
-        res.render('index.html')
+     app.get('/',function(req ,res){
+        // res.render('main.html')
+        /**
+         * json data를 render의 두번째 인자로 전달함으로서 페이지에서 데이터를 사용가능하게함.
+         */
+        res.render('index', {
+            title: "MY PAGE",
+            length: 5
+        });
      });
-     app.get('/about',function(req,res){
-        res.render('about.html');
-    });
+    //  app.get('/about',function(req,res){
+    //     res.render('about.html');
+    // });
 }
--- myExpressApp/server.js
+++ myExpressApp/server.js
@@ -1,15 +1,41 @@
 var express = require('express');
 var app = express();
-var router = require('./routes/main')(app);
+var bodyParser = require('body-parser');
+
+// session module을 이용하여 쿠키 접근 
+var session = require('express-session');
+
+// file read
+var fs = require('fs');
 
 // server가 읽을 수 있도록 html 위치 지정
 app.set('views', __dirname + '/views');
 
 // ejs엔진을 사용하도록 설정
-app.set('view engin', 'ejs');
+app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 var server = app.listen(3000, function() {
     console.log("express server has started on port 3000");
 });
 
 app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( {
+    extended: true
+}));
+
+/**
+ * secret : 쿠키를 임의로 변조하는것을 방지하기 위한 sign (원하는 값을 넣으면 됨)
+ * resave : 세션을 언제나 저장할지 정하는 값 express-session documentation에서는 이 값을 false로 하는 것을 권장하고 필요에 따라 true함
+ * saveUninitialized : uninitialized 세션이란 새로 생겼지만 변경되지 않은 세션을 의미 Documentation에서 이 값을 true로 설정하는 것을 권장.
+ */
+app.use(session({
+    secret: '@#@$MYSIGN#@$#$',
+    resave: false,
+    saveUninitialized: true
+}));
+
+/**
+ * 정적파일 사용 (html에서 사용되는 js 파일,  css파일, img파일)
+ */
+var router = require('./routes/main')(app, fs);
